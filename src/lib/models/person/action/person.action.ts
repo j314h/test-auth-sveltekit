5ff887c5fd9ade9e348d234254ca1eacb139661e
j314h <johnhaimez314@gmail.com>
2022-01-18 21:53:25 +0100

    creation dashboard relier à session

--- src/lib/models/person/action/person.action.ts
+++ src/lib/models/person/action/person.action.ts
@@ -2,7 +2,10 @@ import { goto } from '$app/navigation';
 import { callApi } from '$lib/provider/fetch/fetch.service';
 import { createObjectAsFormData } from '$lib/provider/form/form.service';
 import { EMethodeFetch } from '$lib/types/fetch.type';
-import type { IPersonPublishReceved } from '$lib/types/person.type';
+import type {
+  IPersonPublishReceved,
+  IPersonReceved,
+} from '$lib/types/person.type';
 
 /**
  * create person
@@ -26,4 +29,25 @@ export const createPerson = async (e): Promise<void> => {
   console.log(person);
 
   //goto('/dashboard');
+};
+
+/**
+ * connexion person
+ */
+export const login = async (e): Promise<void> => {
+  // creation formData
+  const formData = createObjectAsFormData(e.target);
+
+  // create
+  const person = await callApi<IPersonReceved>(
+    'api/login.json',
+    EMethodeFetch.POST,
+    formData
+  );
+
+  // si ok on va sur dashbord
+  if (person) {
+    console.log('RESPONSE : ', person);
+    goto('/dashboard');
+  }
 };
--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,1 +1,77 @@
+<script lang="ts">
+  import { session } from '$app/stores';
+  import { createPerson } from '$lib/models/person/action/person.action';
+  import { ERole } from '$lib/types/role.type';
+
+  // les roles pour creation user
+  let roles: ERole[] = [ERole.ROOT, ERole.DEV, ERole.CLIENT];
+
+  // create user input value
+  let valueFirstName = '';
+  let valueLastName = '';
+  let valuePseudo = '';
+  let valueEmailCreate = '';
+  let valuePasswordCreate = '';
+
+  //envoie formulaire create
+  const handlerCreateForm = (e) => {
+    createPerson(e);
+    valueFirstName = '';
+    valueLastName = '';
+    valuePseudo = '';
+    valueEmailCreate = '';
+    valuePasswordCreate = '';
+  };
+</script>
+
 <p>Mon compte</p>
+
+{#if $session.user.role !== 'client'}
+  <!-- création user -->
+  <h1>Creation Users</h1>
+  <form on:submit|preventDefault={handlerCreateForm}>
+    <input
+      type="text"
+      name="firstName"
+      placeholder="firstName"
+      bind:value={valueFirstName}
+    />
+    <input
+      type="text"
+      name="lastName"
+      placeholder="lastName"
+      bind:value={valueLastName}
+    />
+    <input
+      type="text"
+      name="pseudo"
+      placeholder="pseudo"
+      bind:value={valuePseudo}
+    />
+    <input
+      type="text"
+      name="email"
+      placeholder="email"
+      bind:value={valueEmailCreate}
+    />
+    <input
+      type="text"
+      name="password"
+      placeholder="password"
+      bind:value={valuePasswordCreate}
+    />
+    <select name="role" id="role-select">
+      <option value="">-- choisissez un role --</option>
+      {#each roles as role}
+        <option value={role}>{role}</option>
+      {/each}
+    </select>
+    <button>Envoyer</button>
+  </form>
+{/if}
+
+<style>
+  input {
+    display: block;
+  }
+</style>
