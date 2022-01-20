8e76a8905b6e3f2e372a89e479f6e89e45bde90e
David-Caignaert <davidcaignaert@gmail.com>
2022-01-20 14:41:50 +0100

    file.query + dans _apiService.ts ajout du CRUD file

--- src/lib/query/file.query.ts
+++ src/lib/query/file.query.ts
@@ -1,11 +1,33 @@
-import { IFile } from './../types/file.type';
 import { gql } from 'graphql-request';
 
-
+/**
+ * request creation du file
+ */
 export const ReqCreateFile = gql`
-  mutation createFile($new: IFile!) {
+  mutation createFile($new: AssetCreateInput!) {
     createFile(data: $new) {
       id
     }
   }
+`
+
+/**
+ * request modification du file
+ */
+export const ReqUpdateFile = gql`
+mutation updateFile($where: ID!, $data: AssetUpdateInput  ) {
+  updateAsset(where: { id: $where }, data: $data) {
+    id
+  }
+}
+`
+
+/**
+ * resquest effacement du file
+ */
+export const ReqDeleteFile = gql`
+  mutation deleteAsset($where: ID) {
+  deleteAsset(where:{id: $where}){
+  }
+}
 `
