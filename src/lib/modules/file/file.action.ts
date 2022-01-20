06878f95eb3a03b8e2f334b1a5fac59c2fbd3512
j314h <johnhaimez314@gmail.com>
2022-01-20 18:49:02 +0100

    r

--- src/lib/modules/file/file.action.ts
+++ src/lib/modules/file/file.action.ts
@@ -1,8 +1,6 @@
-import { graphqlServiceFile } from './../../provider/graphql/graphql.service';
+import { graphqlServiceFile } from '$lib/provider/graphql/graphql.service';
 import type { IFileCreateReceved } from './file.type';
-import { callApi } from '$lib/provider/fetch/fetch.service';
 import { createObjectAsFormData } from '../../provider/form/form.service';
-import { EMethodeFetch } from '$lib/provider/fetch/fetch.type';
 import { ReqCreateFile } from './file.query';
 
 /**
