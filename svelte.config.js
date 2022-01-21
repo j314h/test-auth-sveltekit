84f17531f09d6c65a92e15ef50edaed7f0389444
j314h <johnhaimez314@gmail.com>
2022-01-21 14:30:41 +0100

    rectification route dans fichier de config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,13 +12,6 @@ const config = {
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    vite: {
-      resolve: {
-        alias: {
-          $routes: resolve('./src/routes'),
-        },
-      },
-    },
   },
 };
 
